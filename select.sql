64191cfa7ccc31ec2b74c05a059e9dd51166e6b6
Nikitich2033 <lyakhovoynikita@gmail.com>
2021-03-14 01:00:44 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -18,8 +18,8 @@ WHERE gender = "Female";
 -- 2. Coaching Report.
 
 SELECT CoachName, CoachSurname, COUNT(idContender)
-FROM Coach, Contender
-WHERE Coach.idCoach = Contender.idCoach;
+FROM Coach LEFT JOIN Contender
+ON Coach.idCoach = Contender.idCoach;
 
 
 -- 3. Coach Monthly Attendance Report
