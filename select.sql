7ad08bfdd38eb9c5f3cc7084ab1e01a3dc1beb98
Nikitich2033 <lyakhovoynikita@gmail.com>
2021-03-14 03:06:56 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -39,7 +39,7 @@ ORDER BY CoachName, CoachSurname;
 #SELECT stageName, MAX(Total_Daily_Salary_Per_Contender.Total_Daily_Salary) as Highest_Total_Daily_Salary
 #FROM (
  
-    SELECT Contender.idContender, dailySalary * COUNT(TVShow.idShow) AS Total_Daily_Salary
+    SELECT Participant.idParticipant,Participant.idContender, dailySalary * COUNT(TVShow.idShow) AS Total_Daily_Salary
     FROM Participant
     LEFT JOIN Contender
     ON Participant.idContender = Contender.idContender
