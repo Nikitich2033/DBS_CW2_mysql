2d38949b61c70391ddb365323082a72e4dcf8d66
Nikitich2033 <lyakhovoynikita@gmail.com>
2021-03-15 17:14:20 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -37,11 +37,11 @@ ORDER BY CoachName, CoachSurname;
 
 -- 4. Most Expensive Contender
 
-    SELECT stageName, MAX(Total_Daily_Salary_Per_Contender) as Highest_Total_Daily_Salary
+    SELECT stageName,CONT_ID, MAX(Total_Daily_Salary_Per_Contender) as Highest_Total_Daily_Salary
     FROM(
-        SELECT stageName, SUM(Participant_Total_Daily_Salary) as Total_Daily_Salary_Per_Contender
+        SELECT stageName, CONT_ID, SUM(Participant_Total_Daily_Salary) as Total_Daily_Salary_Per_Contender
         FROM(
-            SELECT stageName, PART_ID, Total_Daily_Salary_Per_Contender_Participant.Total_Daily_Salary as Participant_Total_Daily_Salary
+            SELECT stageName, PART_ID, CONT_ID,Total_Daily_Salary_Per_Contender_Participant.Total_Daily_Salary as Participant_Total_Daily_Salary
             FROM (
         
                 SELECT stageName, Participant.idParticipant as PART_ID, Participant.idContender as CONT_ID, dailySalary * COUNT(TVShow.idShow) AS Total_Daily_Salary
@@ -56,7 +56,7 @@ ORDER BY CoachName, CoachSurname;
                 GROUP BY PART_ID) TotalByParticipant
                 GROUP BY stageName
     ) TotalByContender
-    WHERE TotalByContender.CONT_ID = TotalByParticipant.CONT_ID
+    WHERE CONT_ID = TotalByParticipant.CONT_ID
     ORDER BY stageName desc limit 1;
 
     
