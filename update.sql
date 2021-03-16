7979baadcf7088c86d9267be3ee124308f7eb11f
Nikitich2033 <lyakhovoynikita@gmail.com>
2021-03-16 18:32:44 +0300

    Update update.sql

--- update.sql
+++ update.sql
@@ -50,7 +50,7 @@ ADD COLUMN LeaveTime TIME NOT NULL;
 
 UPDATE CoachInShow,
         TVShow
-SET ArriveTime = TVShow.startTime - "02:00"
+SET ArriveTime = TVShow.startTime - "02:00:00"
 WHERE CoachInShow.idShow = TVShow.idShow;
 
 
