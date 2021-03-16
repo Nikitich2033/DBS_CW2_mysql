14b82e248c3226c780f4a157d99cefc5a19f524f
Nikitich2033 <lyakhovoynikita@gmail.com>
2021-03-16 23:54:57 +0300

    Change delete because it does not work after update is run

--- delete.sql
+++ delete.sql
@@ -11,8 +11,37 @@
 
 
 -- Remove contender with the lowest total salary
--- A view with total salaries of every contender already exists: TotalByContender.
--- It was made to get the contender with the highest salary.
+
+CREATE OR REPLACE VIEW Total_Daily_Salary_By_idParticipant
+AS
+SELECT stageName, Participant.idParticipant as PART_ID, 
+                Participant.idContender as CONT_ID, 
+                hourlyPayment * COUNT(TVShow.idShow) * 4 AS Total_Daily_Salary
+        FROM Participant
+        LEFT JOIN Contender
+        ON Participant.idContender = Contender.idContender
+        LEFT JOIN ContenderInShow
+        ON Contender.idContender = ContenderInShow.idContender
+        LEFT JOIN TVShow
+        ON TVShow.idShow = ContenderInShow.idShow
+        GROUP BY idParticipant;
+
+-- SELECT TOTAL DAILY SALARY OF EACH PARTICIPANT 
+CREATE OR REPLACE VIEW TotalByParticipant
+AS 
+SELECT stageName, PART_ID, CONT_ID,Total_Daily_Salary_By_idParticipant.Total_Daily_Salary as Participant_Total_Daily_Salary
+FROM Total_Daily_Salary_By_idParticipant
+GROUP BY PART_ID;
+
+CREATE OR REPLACE VIEW TotalByContender
+AS
+-- SELECT THE SUM OF SALARIES FOR EACH CONTENDER
+SELECT stageName, CONT_ID, SUM(Participant_Total_Daily_Salary) as Total_Daily_Salary_Per_Contender
+FROM TotalByParticipant
+-- SELECT TOTAL DAILY SALARIES OF ALL PARTICIPANTS     
+GROUP BY stageName;
+
+
 
 CREATE OR REPLACE VIEW LowestTotalSalary
 AS
