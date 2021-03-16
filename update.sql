a6b6d5df45ceb840332ca70d132f36d1c1ad4c55
Nikitich2033 <lyakhovoynikita@gmail.com>
2021-03-16 18:07:20 +0300

    Update update.sql

--- update.sql
+++ update.sql
@@ -35,6 +35,10 @@ select * from Participant;
 
 -- 2. Add new fields to the attendance table to register when coaches and contenders arrive to and leave the shows.
 
+CREATE OR REPLACE VIEW ShowTimes
+AS
+SELECT idShow, startTime, endTime
+FROM TVShow;
 
 ALTER TABLE CoachInShow
 ADD COLUMN ArriveTime TIME NOT NULL,
